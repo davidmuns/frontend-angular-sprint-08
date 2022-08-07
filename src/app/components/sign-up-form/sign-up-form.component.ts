45f37ebe8c8fdd4cdcb294e5f827f965faf5ec53
David Gomez Muns <davidmuns@yahoo.es>
2022-08-07 20:54:05 +0200

    adding reactive form at sgin up

--- src/app/components/sign-up-form/sign-up-form.component.ts
+++ src/app/components/sign-up-form/sign-up-form.component.ts
@@ -1,6 +1,6 @@
 import { UserService } from './../../services/user.service';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up-form',
@@ -10,17 +10,27 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class SignUpFormComponent implements OnInit {
 
   signUpFormGroup: FormGroup;
-  constructor(private userService: UserService) {
-    this.signUpFormGroup = new FormGroup({
-      email: new FormControl(),
-      password: new FormControl()
-    });
+
+  // constructor(private userService: UserService) {
+  //   this.signUpFormGroup = new FormGroup({
+  //     email: new FormControl(),
+  //     password: new FormControl()
+  //   });
+  // }
+
+  constructor(private userService: UserService, private formBuilder: FormBuilder) {
+    this.signUpFormGroup = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    })
   }
 
   ngOnInit(): void {
   }
 
   public onSubmit() {
+    console.log(this.signUpFormGroup);
+
     this.userService.addUser(this.signUpFormGroup.value);
   }
 
